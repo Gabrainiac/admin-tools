7b6b2506e76ade617d61ad9eb60f98a86ad224dd
Gabrainiac <gayoflodu@gmail.com>
2026-05-24 16:02:28 -0500

    Update db.ts

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,19 +1,33 @@
-import { PrismaClient } from '@prisma/client'
+import pkg from '@prisma/client'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { config } from 'dotenv'
 
 config()
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const createPrismaClient = () => {
-  const connectionString = process.env.DATABASE_URL
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
+const { PrismaClient } = pkg
+
+const globalForPrisma = globalThis as unknown as {
+	prisma: InstanceType<typeof PrismaClient>
 }
 
-export const db: PrismaClient = globalForPrisma.prisma ?? createPrismaClient()
+const createPrismaClient = () => {
+	const connectionString = process.env.DATABASE_URL
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+	const pool = new Pool({
+		connectionString
+	})
 
+	const adapter = new PrismaPg(pool)
+
+	return new PrismaClient({
+		adapter
+	})
+}
+
+export const db =
+	globalForPrisma.prisma ?? createPrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+	globalForPrisma.prisma = db
+}
