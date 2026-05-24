e41e6c03ed54163eaef03f8a616cdd077a7f4ed8
Gabriel <gayoflodu@gmail.com>
2026-05-24 17:24:14 -0500

    fix railway postgres

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,18 +1,21 @@
-import { PrismaClient } from '@prisma/client'
+import pkg from '@prisma/client'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
+import { config } from 'dotenv'
+
+config()
+
+const { PrismaClient } = pkg
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: InstanceType<typeof PrismaClient>
 }
 
-function createPrismaClient() {
-  if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL missing')
-  }
+const createPrismaClient = () => {
+  const connectionString = process.env.DATABASE_URL
 
   const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString
   })
 
   const adapter = new PrismaPg(pool)
